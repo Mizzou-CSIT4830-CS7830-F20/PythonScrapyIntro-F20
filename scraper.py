57d01b8678b95e705aa29643cdac264bef08436e
Professor Wergeles <wergelesn@missouri.edu>
2019-10-28 22:56:31 -0500

    Now we are grabbing other data from the page. You will see I am using XPATH selectors instead of just CSS selectors. In some cases, CSS selectors are not strong enough to get the data, as a result we need to use XPATH selectors ... it all depends on what you are trying to pull. Sometimes you can use all CSS selectors depending on how the page is structured ... each page is different

--- scraper.py
+++ scraper.py
@@ -18,7 +18,13 @@ class BrickSetSpider(scrapy.Spider):
         for brickset in response.css(SET_SELECTOR):
 
             NAME_SELECTOR = "h1 ::text"
+            PIECES_SELECTOR = ".//dl[dt/text() = 'Pieces']/dd/a/text()"
+            MINIFIGS_SELECTOR = ".//dl[dt/text() = 'Minifigs']/dd[2]/a/text()"
+            IMAGE_SELECTOR = "img ::attr(src)"
 
             yield {
                 'name': brickset.css(NAME_SELECTOR).extract_first(),
+                'pieces': brickset.xpath(PIECES_SELECTOR).extract_first(),
+                'minifigs': brickset.xpath(MINIFIGS_SELECTOR).extract_first(),
+                'image': brickset.css(IMAGE_SELECTOR).extract_first()
             }
